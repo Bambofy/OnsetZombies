98e5eb32cfda351a3333ab5c225e10d3142d0e96
Bambofy <rbamfordz@gmail.com>
2019-12-27 13:57:15 +0000

    clothes

--- client.lua
+++ client.lua
@@ -4,8 +4,6 @@ end)
 
 AddEvent("OnNPCStreamIn", function(npc)
     if GetNPCPropertyValue(npc, "IS_ZOMBIE") ~= nil then
-        local clothesId = GetNPCPropertyValue(npc, "CLOTHES_ID")
-        SetNPCClothingPreset(npc, math.random(21, 22)))
-        print("SET NPC CLOTHES", clothesId)
+        SetNPCClothingPreset(npc, 21)
     end
 end)
