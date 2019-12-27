0856d93fa31cfe8eaa3b165589c6249cdddb6ffe
Richard Bamford <rbamfordz@gmail.com>
2019-12-27 09:34:28 +0000

    added npc model changing - johnny2x2

--- client.lua
+++ client.lua
@@ -1,3 +1,9 @@
 AddEvent("OnPackageStart", function()
     print("[CL] Zombies initialized...")
 end)
+
+AddEvent("OnNPCStreamIn", function(npc)
+    if GetNPCPropertyValue(npc, "IS_ZOMBIE") ~= nil then
+        SetNPCClothingPreset(npc, 21)
+    end
+end)
